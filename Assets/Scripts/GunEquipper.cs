0031c91e7f5eab89606ae0f86a0dd9df1e26efe4
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-10-22 10:07:46 -0400

    Chapter 9 - Ammo Animation - Chapter Completed

--- Assets/Scripts/GunEquipper.cs
+++ Assets/Scripts/GunEquipper.cs
@@ -9,6 +9,9 @@ public class GunEquipper : MonoBehaviour
     public GameObject assaultRifle;
     public GameObject shotgun;
     GameObject activeGun;
+
+    [SerializeField]
+    GameUI gameUI;
     void Start()
     {
         activeWeaponType = Constants.Pistol;
@@ -29,13 +32,19 @@ public class GunEquipper : MonoBehaviour
         {
             loadWeapon(pistol);
             activeWeaponType = Constants.Pistol;
+            gameUI.UpdateReticle();
+        }
+        else if (Input.GetKeyDown("2"))
+        { 
             loadWeapon(assaultRifle);
             activeWeaponType = Constants.AssaultRifle;
+            gameUI.UpdateReticle();
         }
         else if (Input.GetKeyDown("3"))
         {
             loadWeapon(shotgun);
-            activeWeaponType = Constants.Shotgun;
+            activeWeaponType = Constants.Shotgun; 
+            gameUI.UpdateReticle();
         }
     }
     public GameObject GetActiveWeapon()
