060021b72dfc4768a52231201faabed29673e9d5
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-11-14 14:35:39 -0500

    Chapter 10 - Robot Spawning

---
+++ Assets/Scripts/RobotSpawn.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSpawn : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] robots;
+    private int timesSpawned;
+    private int healthBonus = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void SpawnRobot()
+    {
+        timesSpawned++;
+        healthBonus += 1 * timesSpawned;
+        GameObject robot = Instantiate(robots[Random.Range(0, robots.Length)]);
+        robot.transform.position = transform.position;
+        robot.GetComponent<Robot>().health += healthBonus;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
