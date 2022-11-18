80f05adeba698f43af088893176165a994234cf6
Ricardo-Git <rlm.ricohs10@gmail.com>
2022-11-18 15:07:43 -0500

    Chapter 11 - Adding a Main menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // 1
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Battle");
+    }
+    // 2
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
